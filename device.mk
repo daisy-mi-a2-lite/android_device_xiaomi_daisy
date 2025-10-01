36cb3517a5ecbdfa12334f2471acabc225ca268a
Al Mahmud <73630234+mdalmahmud023@users.noreply.github.com>
2025-10-01 04:03:49 +0000

    daisy: add SettingsResDaisy overlay and update device.mk

--- device.mk
+++ device.mk
@@ -13,7 +13,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    DaisyFrameworkOverlay
+    DaisyFrameworkOverlay \
+    SettingsResDaisy \
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
